8a28c0c02c93f1b73661a47eb98f4f48f295d805
einargudnig <einargudnig@gmail.com>
2019-01-20 22:35:33 +0000

    Prófa mig áfram með app.js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-/* todo, setja upp express */
+const express = require('express'),
+        path = require('path'),
+        lectures = require('./lectures.js'),
+        app = express(),
+        port = 3000;
+
+ //Use EJS
+ app.set('view engine', 'ejs');
+ 
+ app.set('views', path.join(__dirname, 'views'));
+
+ app.use(express.static('public'));
+
+ //Lecturelist
+ app.use('/', lectures);
+ app.use('/html', lectures);
+ app.use('/css', lectures);
+ app.use('/js', lectures);
+
+ //start server
+ app.listen(port, () => {
+     console.log('allt í góðu');
+ });
+
