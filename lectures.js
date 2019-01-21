43060fd2946c7d06d5e42d07a776f8c62a8a1fd6
einargudnig <einargudnig@gmail.com>
2019-01-21 14:49:54 +0000

    Annað commit, fikt með EJS og fleira

--- lectures.js
+++ lectures.js
@@ -1,20 +1,31 @@
-const express = require('express');
+const express = require('express'),
+        util = require('util'),
+        fs = require('fs');
 
 const router = express.Router();
+const lectures_json_path = './lectureData/lectures.json';
+
+const readFilePromise = util.promisify(fs.readFile);
 
 function catchErrors(fn) {
   return (req, res, next) => fn(req, res, next).catch(next);
 }
 
 async function list(req, res) {
-  /* todo útfæra */
+  const json_file = await readJSONasync(lectures_json_path);
+  res.render("pages/list", { json: json_file, slug: req.originalUrl });
 }
 
 async function lecture(req, res, next) {
   /* todo útfæra */
 }
 
+
+
 router.get('/', catchErrors(list));
+router.get('/html', catchErrors(list));
+router.get('/css', catchErrors(list));
+router.get('/js', catchErrors(list));
 router.get('/:slug', catchErrors(lecture));
 
 module.exports = router;
