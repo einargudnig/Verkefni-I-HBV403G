9a7582a6eddd0b507e376c94359694a6878c2b76
einargudnig <einargudnig@gmail.com>
2019-01-23 23:03:31 +0000

    jajaja

--- lectures.js
+++ lectures.js
@@ -6,11 +6,9 @@ const readFile = util.promisify(fs.readFile);
 
 const router = express.Router();
 
-async function lesaskra() {
-  const texti = await readFile('./lectures.json');
-
-  const json = JSON.parse(texti);
-
+async function lesaJSON() {
+  const file = await readFile('./lectures.json');
+  const json = JSON.parse(file);
   return json;
 }
 
@@ -21,22 +19,34 @@ function catchErrors(fn) {
 async function list(req, res) {
   //lesa fyrirlestrana inn og birta
   const title = 'Fyrirlestrar';
-  const data = await lesaskra();
+  const data = await lesaJSON();
   const { lectures } = data;
-  console.log(lectures);
+  //console.log(lectures);
 
   res.render('index', { title, lectures });
 }
-
+//console.log('banani');
+//console.log('annar banani');
 async function lecture(req, res, next) {
-  const title = 'Banani';
-  //const data = await lesaskra();
-  //const { lectures } = data;
-  console.log(lectures);
+  const { slug } = req.params;
+  const data = await lesaJSON();
+  const foundContent = data.lectures.find(a => a.slug === slug);
+  console.log(foundContent);
 
-  res.render('lecture', { title, lectures });
+  if (!foundContent) {
+    return next();
+  }
+  const { title } = foundContent;
+  const { category } = foundContent;
 
+  const html = item.createContent(foundContent.content);
+  return res.render('lecture', { title, html, category, lecture: foundContent });
 }
+
+
+
+
+
 
 router.get('/', catchErrors(list));
 router.get('/:slug', catchErrors(lecture));
